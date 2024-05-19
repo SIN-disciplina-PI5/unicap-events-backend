94551bac2e65ab4175c44d9e24ff926a37c310bf
Elizabeth Kristen <elizabeth.kristen@hotmail.com>
2024-05-19 19:39:38 +0000

    fix: gatilho para envio de certificado

--- controllers/AccreditationController.js
+++ controllers/AccreditationController.js
@@ -2,9 +2,12 @@ const knex = require('knex');
 const knexFile = require('../knexfile.js');
 const db = knex(knexFile);
 
+const createCertificate = require('../service/createCertificate');
+const sendEmail = require('../service/sendEmail');
+const fs = require('fs');
+
 // Obter um ticket específico por ID
 exports.show = async (req, res) => {
-  console.log(res)
   const codigoIngresso = req.params.id;
   try {
     const codigo = await db('tickets').where('codigo_ingresso', codigoIngresso ).where('status', 'aprovado').first();
@@ -25,20 +28,56 @@ exports.update = async (req, res) => {
   const codigo_ingresso = req.params.id;
 
   try {
-    // Verificar se o ticket existe e se está atribuido a algum usuario
-    const existingTicket = await db('tickets').where('codigo_ingresso', codigo_ingresso).whereNotNull('user_id').first();
+    // Verificar se o ticket existe e se está atribuído a algum usuário
+    const ticket = await db('tickets')
+      .join('users', 'tickets.user_id', '=', 'users.id')
+      .join('sub_events', 'tickets.sub_event_id', '=', 'sub_events.id')
+      .where('tickets.codigo_ingresso', codigo_ingresso)
+      .where('status', 'reservado')
+      .whereNotNull('tickets.user_id')
+      .select(
+        'tickets.*', 
+        'users.name as user_name', 
+        'users.email as user_email', 
+        'sub_events.name as sub_event_name'
+      )
+      .first();
 
-    if (!existingTicket) {
-      return res.status(404).json({ error: 'Ticket não encontrado ou não tem usuario atrelado ao ingresso' });
+    if (!ticket) {
+      return res.status(404).json({ error: 'Ticket não encontrado ou não tem usuário atrelado ao ingresso' });
     }
-    
-    // Atualizar o ticket no banco de dados
-    await db('tickets').where({ codigo_ingresso: codigo_ingresso }).update({status: 'aprovado'});
 
-    res.json({ success: true, message: 'alterado com sucesso'});
+    // Gerar e enviar o certificado
+    await generateAndSendCertificate(ticket.user_name, ticket.sub_event_name, ticket.user_email, ticket.codigo_ingresso)
+      .then(() => console.log('Certificate sent successfully!'))
+      .catch(err => {
+        console.error('Error sending certificate:', err);
+        throw err; // Lança o erro para ser tratado no bloco catch
+      });
+
+    // Atualizar o ticket no banco de dados
+    await db('tickets').where({ codigo_ingresso: codigo_ingresso }).update({ status: 'aprovado' });
+
+    res.json({ success: true, message: 'Ticket atualizado e certificado enviado com sucesso' });
 
   } catch (error) {
-    console.error('Erro ao atualizar o evento:', error);
+    console.error('Erro ao atualizar o ticket:', error);
     res.status(400).json({ error: 'Erro ao credenciar', details: error });
   }
 };
+
+async function generateAndSendCertificate(name, course, email, codigoIngresso) {
+  const certificatePath = certificado - ${name}.pdf;
+
+  // Cria o certificado
+  createCertificate(name, course, codigoIngresso);
+
+  // Espera a criação do arquivo
+  await new Promise(resolve => setTimeout(resolve, 1000)); // Ajuste conforme necessário
+
+  // Envia o email com o certificado
+  await sendEmail(email, 'Seu Certificado do Curso', 'Essa é sua certificação! Parabéns!', certificatePath);
+
+  // Remove o arquivo após envio
+  fs.unlinkSync(certificatePath);
+}
